8e5c0255e733c38ceffc856b1cb6ccf1c14baf70
Marcofa87 <marco.falasca87@gmail.com>
2025-12-16 12:36:40 +0100

    inserito pattern per obbligatorietà di compilazione dei campi

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface FormData {
   name: string;
@@ -13,48 +13,82 @@ interface FormData {
 }
 
 export default function Home() {
-  const { register, handleSubmit } = useForm<FormData>();
-  console.log(register);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
         <input
-          {...register("name")}
+          {...register("name", {
+            required: true,
+            minLength: 3,
+            maxLength: 20,
+            pattern: /^[a-zA-Z]+$/,
+          })}
           type="text"
           placeholder="Enter your name"
         />
         <input
-          {...register("email")}
+          {...register("email", {
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+          })}
           type="email"
           placeholder="Enter your email"
         />
         <input
-          {...register("password")}
+          {...register("password", {
+            required: true,
+            minLength: 8,
+            maxLength: 20,
+            pattern: /^[a-zA-Z0-9]+$/,
+          })}
           type="password"
           placeholder="Enter your password"
         />
         <input
-          {...register("age")}
+          {...register("age", { required: true, min: 18, max: 100 })}
           type="number"
           placeholder="Enter your age"
         />
         <input
-          {...register("dateOfBirth")}
+          {...register("dateOfBirth", {
+            required: true,
+            min: new Date().toISOString().split("T")[0],
+            max: new Date().toISOString().split("T")[0],
+          })}
           type="date"
           placeholder="Enter your date of birth"
         />
         <input
-          {...register("time")}
+          {...register("time", {
+            required: true,
+            pattern: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/,
+          })}
           type="time"
           placeholder="Enter your time"
         />
         <input
-          {...register("phoneNumber")}
+          {...register("phoneNumber", {
+            required: true,
+            pattern: /^[0-9]{10}$/,
+          })}
           type="tel"
           placeholder="Enter your phone number"
         />
         <input
-          {...register("website")}
+          {...register("website", {
+            required: true,
+            pattern: /^https?:\/\/[^\s/$.?#].[^\s]*$/i,
+          })}
           type="url"
           placeholder="Enter your website"
         />
